cf4149b160d540f6a1850ee3c53ea8b43ac264eb
Deepak Sharma <98380079+iamdeepakpandit@users.noreply.github.com>
2023-09-16 15:50:04 +0530

    Script2.js

--- Script2.js
+++ Script2.js
@@ -1,14 +1,14 @@
-const text = "Developed By DEEPAK SHARMA"; // Text to be typed
-const speed = 100; // Typing speed (in milliseconds)
-const textElement = document.getElementById("text"); // Get the text element
+const text = "Developed By DEEPAK SHARMA";
+const speed = 100; 
+const textElement = document.getElementById("text"); 
 
 let i = 0;
 function typeWriter() {
 	if (i < text.length) {
-		textElement.innerHTML += text.charAt(i); // Add the next character
+		textElement.innerHTML += text.charAt(i);
 		i++;
-		setTimeout(typeWriter,  speed); // Call the function again after the specified speed
+		setTimeout(typeWriter,  speed);
 	}
 }
 
-typeWriter(); // Call the function to start the animation
+typeWriter();
